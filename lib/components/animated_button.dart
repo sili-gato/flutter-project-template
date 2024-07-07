78b67bd6e806a73f62a94f30d32ddf791b4b8450
Vedaansh1107 <vedaansh.vijaywargiya@gmail.com>
2024-07-07 17:09:36 +0530

    Improved animated_button.dart

--- lib/components/animated_button.dart
+++ lib/components/animated_button.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_template/components/regular_text_widget.dart';
-import '../constants/app_colors.dart';
-
 class AnimatedButton extends StatefulWidget {
-  String buttonText;
+  Widget child;
   VoidCallback? onButtonPressed;
   Color buttonColor;
-  Color textColor;
   AnimatedButton(
       {super.key,
-      required this.buttonText,
-      this.onButtonPressed,
-      this.buttonColor = Colors.black,
-      this.textColor = AppColors.mainTextColor1});
+        required this.child,
+        this.onButtonPressed,
+        this.buttonColor = Colors.black,});
   @override
   State<AnimatedButton> createState() => _AnimatedButtonState();
 }
@@ -21,9 +16,16 @@ class _AnimatedButtonState extends State<AnimatedButton> {
   double bottomPadding = 6;
   @override
   Widget build(BuildContext context) {
+    Color _hslRelativeColor({double h = 0.0, s = 0.0, l = 0.0}) {
+      final hslColor = HSLColor.fromColor(widget.buttonColor);
+      h = (hslColor.hue + h).clamp(0.0, 360.0);
+      s = (hslColor.saturation + s).clamp(0.0, 1.0);
+      l = (hslColor.lightness + l).clamp(0.0, 1.0);
+      return HSLColor.fromAHSL(hslColor.alpha, h, s, l).toColor();
+    }
     return GestureDetector(
       onTap: () {
-        widget.onButtonPressed;
+        widget.onButtonPressed?.call();
       },
       onTapDown: (value) => setState(() {
         bottomPadding = 0;
@@ -35,17 +37,14 @@ class _AnimatedButtonState extends State<AnimatedButton> {
         duration: const Duration(milliseconds: 100),
         padding: EdgeInsets.only(bottom: bottomPadding),
         decoration: BoxDecoration(
-            color: Colors.grey, borderRadius: BorderRadius.circular(10)),
+            color: _hslRelativeColor(s: -0.20, l: -0.20), borderRadius: BorderRadius.circular(10)),
         child: Container(
           padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
           decoration: BoxDecoration(
             color: widget.buttonColor,
             borderRadius: BorderRadius.circular(10),
           ),
-          child: RegularTextWidget(
-            textToDisplay: widget.buttonText,
-            textColor: widget.textColor,
-          ),
+          child: widget.child,
         ),
       ),
     );
